d1c79128224a28d9e1c6429524db543e34d35a1a
MassiWR <massi.wararoodi@chasacademy.se>
2023-05-08 22:56:25 +0200

    Add window form design

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,20 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtCmFt = new System.Windows.Forms.TextBox();
+            this.txtInch = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblWeightCat = new System.Windows.Forms.Label();
+            this.lblNormalBMI = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(129, 34);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(125, 30);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.CausesValidation = false;
+            this.lblHeight.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(31, 81);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(119, 24);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "Height";
+            this.lblHeight.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtCmFt
+            // 
+            this.txtCmFt.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCmFt.Location = new System.Drawing.Point(129, 81);
+            this.txtCmFt.Name = "txtCmFt";
+            this.txtCmFt.Size = new System.Drawing.Size(58, 30);
+            this.txtCmFt.TabIndex = 3;
+            // 
+            // txtInch
+            // 
+            this.txtInch.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInch.Location = new System.Drawing.Point(193, 81);
+            this.txtInch.Name = "txtInch";
+            this.txtInch.Size = new System.Drawing.Size(61, 30);
+            this.txtInch.TabIndex = 4;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(129, 133);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(125, 28);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "Calculate";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Your BMI";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBMI.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBMI.Location = new System.Drawing.Point(190, 235);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(219, 23);
+            this.lblBMI.TabIndex = 7;
+            this.lblBMI.Text = "label3";
+            this.lblBMI.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Weight Category";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblWeightCat
+            // 
+            this.lblWeightCat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWeightCat.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeightCat.Location = new System.Drawing.Point(190, 283);
+            this.lblWeightCat.Name = "lblWeightCat";
+            this.lblWeightCat.Size = new System.Drawing.Size(219, 23);
+            this.lblWeightCat.TabIndex = 9;
+            this.lblWeightCat.Text = "label5";
+            this.lblWeightCat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblNormalBMI
+            // 
+            this.lblNormalBMI.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNormalBMI.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNormalBMI.ForeColor = System.Drawing.Color.Green;
+            this.lblNormalBMI.Location = new System.Drawing.Point(190, 355);
+            this.lblNormalBMI.Name = "lblNormalBMI";
+            this.lblNormalBMI.Size = new System.Drawing.Size(219, 23);
+            this.lblNormalBMI.TabIndex = 10;
+            this.lblNormalBMI.Text = "label3";
+            this.lblNormalBMI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Your BMI";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(828, 455);
+            this.ClientSize = new System.Drawing.Size(888, 638);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblNormalBMI);
+            this.Controls.Add(this.lblWeightCat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtInch);
+            this.Controls.Add(this.txtCmFt);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
             this.Name = "MainForm";
             this.Text = "BMICalculator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtCmFt;
+        private System.Windows.Forms.TextBox txtInch;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblWeightCat;
+        private System.Windows.Forms.Label lblNormalBMI;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -16,5 +16,25 @@ namespace BMICalculator
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
